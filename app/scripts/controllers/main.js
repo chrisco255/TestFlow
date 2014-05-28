d0c3fde2b26f252c572bd52612356f9244b1db80
unknown <ChrisC@CHRISCW7X6540.us.corp>
2014-05-28 11:02:43 -0400

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('testFlowApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
