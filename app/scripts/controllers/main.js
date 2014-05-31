4eb60ec20d7ec1b38108e42ecde0e8df3c268074
unknown <ChrisC@CHRISCW7X6540.us.corp>
2014-05-31 05:11:47 -0400

    nested directives implemented

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,57 @@
 'use strict';
 
 angular.module('testFlowApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function ($scope) {
+		function Node(content, children) {
+			var childNodes = [];
+			if(children) {
+				for(var i = 0; i < children.length; i++) {
+					childNodes[i] = new Node(children[i]);
+				}
+			}
+			return {
+				content: content || "",
+				children: childNodes
+			};
+		}
+
+		function NodeCtrl($scope, content) {
+			$scope.content = content || "";
+			$scope.children = [];
+		}
+
+		function appendNode() {
+			$scope.root.children.push(new Node(""));
+		}
+
+		$scope.enterKeyHandler = function() {
+			appendNode();
+		};
+
+		function setupDefaultNodes(root) {
+			root.children.push.apply(root.children, [
+				new Node("This is", ["child1", "child2"]),
+				new Node("a test", ["child1"]),
+				new Node("to see how"),
+				new Node("well this works")
+			]);
+		}
+
+		$scope.root = new Node();
+		setupDefaultNodes($scope.root);
+
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+
+		$scope.removeEnd = function() {
+			$scope.awesomeThings.splice(1);
+		};
+
+		$scope.removeAll = function() {
+			$scope.awesomeThings = [];
+		};
+
+	});
