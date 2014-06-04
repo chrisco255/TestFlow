a9e06142150e3945e0fd25b1133b813177b39c13
unknown <ChrisC@CHRISCW7X6540.us.corp>
2014-06-04 09:44:21 -0400

    changes

--- app/scripts/directives/node.js
+++ app/scripts/directives/node.js
@@ -7,6 +7,7 @@ angular.module('testFlowApp')
 	    replace: true,
 	    template: '<li><content contenteditable="true" ng-model="node.content"></content></li>',
 	    link: function(scope, element, attrs, ctrl) {
+
 	    	if (angular.isArray(scope.node.children)) {
 					element.append("<nodecollection nodecollection='node.children'></nodecollection>"); 
 					$compile(element.contents())(scope);
