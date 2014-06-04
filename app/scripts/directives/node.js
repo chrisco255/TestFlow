f6c451bdd8822a8de8324e4cb1c107bc03c31d0c
unknown <ChrisC@CHRISCW7X6540.us.corp>
2014-06-04 13:21:30 -0400

    added plus/minus buttons on hover

--- app/scripts/directives/node.js
+++ app/scripts/directives/node.js
@@ -5,8 +5,16 @@ angular.module('testFlowApp')
   	return {
   		restrict: 'E',
 	    replace: true,
-	    template: '<li><content contenteditable="true" ng-model="node.content"></content></li>',
+	    template: '<li class="expanded"><span class="expandable"><i class="fa fa-plus"></i></span><span class="collapsible"><i class="fa fa-minus"></i></span><content contenteditable="true" ng-model="node.content"></content></li>',
 	    link: function(scope, element, attrs, ctrl) {
+	    	element
+		    	.mouseenter(function() {
+		    		$(".collapsible, .expandable").hide();
+		    		element.children(".expandable").show();
+		    	})
+		    	.mouseleave(function() {
+		    		element.children(".collapsible, .expandable").hide();
+	    		});
 	    	if (angular.isArray(scope.node.children)) {
 					element.append("<nodecollection nodecollection='node.children'></nodecollection>"); 
 					$compile(element.contents())(scope);
